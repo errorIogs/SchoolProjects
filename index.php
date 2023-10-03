effd6a20d81db7f3aadcc23e9227c497fb38dfc0
errorIogs <74736578+errorIogs@users.noreply.github.com>
2023-10-04 00:38:27 +0200

    update

--- index.php
+++ index.php
@@ -6,10 +6,13 @@ $dbname = "database";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-
+$sqlinfo = "INSERT INTO users (`ID`, `Username`, `Password`, `Country`) VALUES ('','knosk1337','amazed','Sweden')";
+$conn->query($sqlinfo);
+echo 'test';
+$conn->query($sqlinfo);
 // Check connection
 if($conn){
     echo 'successful';
-    
+
 }
 ?>
